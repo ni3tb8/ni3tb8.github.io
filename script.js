b9a9e4e1495a176836ab66c86ef8fe03e52b6b9a
ni3tb8 <ni3tb8@gmail.com>
2025-01-06 20:00:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ window.addEventListener("offline", function() {
 window.addEventListener('DOMContentLoaded', () => {
     setTimeout(function () {
     document.documentElement.style.visibility = 'visible'; // Pokazuje stronę dopiero po załadowaniu
-}, 300);  
+}, 1000);  
 });
 
 // Funkcja do inicjalizacji
@@ -42,9 +42,9 @@ function initializePage() {
         setTimeout(function () {
             loadingScreen.classList.add('hidden');
             document.getElementById('content').style.display = 'block';
-        }, 400);  
+        }, 800);  
 
-    }, 200); // Opóźnienie 0.5s, aby wyświetlić ekran ładowania
+    }, 400); // Opóźnienie 0.5s, aby wyświetlić ekran ładowania
 }
 
 // document.addEventListener('DOMContentLoaded', function () {
